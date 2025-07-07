28dcc7170d5595e6762056ca11a632782084ba73
HuidongDu <18536395062@163.com>
2025-07-07 22:24:28 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -2,10 +2,7 @@ import warnings
 warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
-# BILIBILI UP 魔傀面具
-# 推理参数官方详解链接：https://docs.ultralytics.com/modes/predict/#inference-sources:~:text=of%20Results%20objects-,Inference%20Arguments,-model.predict()
 
-# 预测框粗细和颜色修改问题可看<使用说明.md>下方的<YOLOV8源码常见疑问解答小课堂>第六点
 
 if __name__ == '__main__':
     model = YOLO(r'D:\ssj\ultralytics-main(ssj)\runs\dataset9\yolov11\weights\best.pt') # select your model.pt path
