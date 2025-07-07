63467f20d99e0cc08de6c757c454cf3bf7fd3f16
HuidongDu <18536395062@163.com>
2025-07-07 22:15:54 +0800

    first
    first

--- train.py
+++ train.py
@@ -16,11 +16,7 @@ if __name__ == '__main__':
                 workers=4, # Windows下出现莫名其妙卡主的情况可以尝试把workers设置为0
                 device='0',
                 optimizer='SGD', # using SGD
-                # patience=0, # set 0 to close earlystop.
-                # resume=True, # 断点续训,YOLO初始化时选择last.pt,例如YOLO('last.pt')
-                # amp=False, # close amp
-                # fraction=0.2,
+
                 project='runs/dataset12',
                 name='yolov10',
-                conf=0.1,
                 )
